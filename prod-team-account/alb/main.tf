a93055fd6e37bcbaa3708bc5dbf2c5edfaa67d19
Choi Yunho <cyunho62100@dgu.ac.kr>
2025-07-07 11:19:31 +0900

    vpc 설정

--- prod-team-account/alb/main.tf
+++ prod-team-account/alb/main.tf
@@ -71,6 +71,7 @@ resource "aws_wafv2_web_acl" "alb_waf" {
 }
 
 # ALB
+#tfsec:ignore:aws-alb-not-public
 resource "aws_lb" "alb" {
   name               = "${var.project_name}-alb"
   internal           = false
@@ -130,7 +131,7 @@ resource "aws_lb_listener" "https" {
     load_balancer_arn = aws_lb.alb.arn
     port              = 443
     protocol          = "HTTPS"
-    ssl_policy        = "ELBSecurityPolicy-2016-08"
+    ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
     certificate_arn   = data.terraform_remote_state.acm.outputs.certificate_arn
     default_action {
         type = "forward"
