ca4ab9e034ee3e1efc54c8b23655878482f8d62a
Choi Yunho <cyunho62100@dgu.ac.kr>
2025-07-07 11:28:37 +0900

    acm backend 수정

--- prod-team-account/acm/backend.tf
+++ prod-team-account/acm/backend.tf
@@ -4,5 +4,6 @@ terraform {
     key            = "prod-team-account/acm/terraform.tfstate" 
     region         = "ap-northeast-2"
     dynamodb_table = "s3-prod-lock"
+    encrypt        = true
   }
 }
